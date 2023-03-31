96d51c4589c577dc6c964690cceb031510b77b82
Julian <julianesteban746@gmail.com>
2023-03-31 20:34:11 -0300

    Ejercicios en clase 31/3

---
+++ ejerciciojava313-gonzalezjulian/EjemploEnClaseConComentarios.java
@@ -0,0 +1,21 @@
+public class PrimerEjercicioVariables{
+	public static void main (String[] args) {
+//*indica el tipo de objeto que regresa la función. En este caso la función no regresa ningún valor por eso es void*// {
+	//*trayendo las variables*//
+	int NumA = 50; 
+//*tipo de variable, nombre de variable y variable*//
+	int NumB = 60;
+//* con punto y coma se separan las variables*//
+	int resultado = 0;
+	//*mostrando en pantalla las variables*//
+	System.out.println("--Las Variables--");
+	System.out.println ("--El valor de NumA es" + NumA);
+//*imprime tal cual lo que esta entre comilla y traes la variable con  + *//
+	System.out.println ("--El valor de NumB es" + NumB);
+	System.out.println ("--El valor del resultado es" + resultado);
+//*operacion suma de variables*//
+	resultado = NumA + NumB
+//*luego  de copilar solo pones el nombre de archivo sin su extension*//
+	
+	
+	}}
---
+++ ejerciciojava313-gonzalezjulian/EjerciciosVariables31.java
@@ -0,0 +1,13 @@
+public class EjerciciosVariables31{ 
+	public static void main (String[] args)  { 
+		int A = 10; 
+		int B = 20;
+		int C = 30;
+		int D = 40;
+	System.out.println("Resultados");
+	System.out.println("El valor de B es: " + C);
+	System.out.println ("El valor de C es: " + A);
+	System.out.println ("El valor de A es: " + D);
+	System.out.println ("El valor de D es: " + B);
+		
+} }
