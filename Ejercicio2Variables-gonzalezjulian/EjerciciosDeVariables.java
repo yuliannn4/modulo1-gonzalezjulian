fe8a8eedfb21d162c40469759852f237e667d082
Julian <julianesteban746@gmail.com>
2023-04-03 21:36:07 -0300

    Ejercicios de variables

---
+++ Ejercicio2Variables-gonzalezjulian/EjerciciosDeVariables.java
@@ -0,0 +1,9 @@
+public class EjerciciosDeVariables{
+	public static void main (String [] args) {
+		int Var1 = 30;
+		int Var2 = 50;
+		int Var3 = Var1 + Var2;
+	System.out.println("El resultado es " + Var3);
+		
+	}
+	}
