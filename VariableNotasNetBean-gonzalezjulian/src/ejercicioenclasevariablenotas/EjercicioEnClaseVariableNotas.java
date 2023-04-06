5872ffaebffe932492dd6c3f15c6b1229a35ce7e
Julian <julianesteban746@gmail.com>
2023-04-05 21:07:48 -0300

    Variables en NetBeans

---
+++ VariableNotasNetBean-gonzalezjulian/src/ejercicioenclasevariablenotas/EjercicioEnClaseVariableNotas.java
@@ -0,0 +1,24 @@
+package ejercicioenclasevariablenotas;
+
+/**
+ *
+ * @author Julian
+ */
+public class EjercicioEnClaseVariableNotas {
+
+
+    public static void main(String[] args) {
+     int Nota= 5;
+        
+     
+        System.out.println("Nombre del Alumno: Julian Gonzalez");
+        System.out.println("Su Nota es " + Nota);
+            
+            if (Nota >= 6){
+        System.out.println("Felicidades! Usted aprobo");
+        }   else {
+        System.out.println("Lamentamos informar que usted NO aprobo");
+        }
+}
+    
+    }
