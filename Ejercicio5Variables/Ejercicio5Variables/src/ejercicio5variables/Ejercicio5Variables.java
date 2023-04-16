360dbcc98e9c743932f2d7bf5edadf6d860a4bb6
Julian <julianesteban746@gmail.com>
2023-04-16 19:53:05 -0300

    Ejercicio 5 Variables

---
+++ Ejercicio5Variables/Ejercicio5Variables/src/ejercicio5variables/Ejercicio5Variables.java
@@ -0,0 +1,83 @@
+
+package ejercicio5variables;
+
+public class Ejercicio5Variables {
+
+  
+    public static void main(String[] args) {
+        
+      // Declaro las variables 
+        final int X = 5;
+        final int Y = 9;
+        final double N = 4.5;
+        final double M = 9.4;
+
+      // Variable resultado
+        double resultado = 0;
+        
+     //muestro los valores en pantalla
+            System.out.println(" --------------------------- ");
+            System.out.println("|  VALORES DE LAS VARIABLES |");
+            System.out.println(" --------------------------- ");
+            System.out.println(" El valor de X es de: " + X);
+            System.out.println(" El valor de Y  es de: " + Y);
+            System.out.println(" El valor de N es de: " + N);
+            System.out.println(" El valor de M  es de: " + M);
+            
+            System.out.println(" ---------------------------- ");
+            System.out.println("|  OPERACIONES CON VARIABLES |");
+            System.out.println(" ---------------------------- ");
+
+            
+            resultado = X + Y;
+            System.out.println("El resultado  de " + X + " + " + Y + " es de: " + resultado );
+            resultado = X - Y;
+            System.out.println("El resultado  de " + X + " - " + Y + " es de: " + resultado );
+            resultado = X * Y;
+            System.out.println("El resultado  de " + X + " * " + Y + " es de: " + resultado );
+            resultado = X / Y;
+            System.out.println("El resto  de " + X + " / " + Y + " es de: " + resultado );
+            resultado = X % Y;
+            System.out.println("El resto  de " + X + " % " + Y + " es de: " + resultado );
+            resultado = N + M;
+            System.out.println("El resultado  de " + N + " + " + M + " es de: " + resultado );
+            resultado = N - M;
+            System.out.println("El resultado  de " + N + " - " + M + " es de: " + resultado );
+            resultado = N * M;
+            System.out.println("El resultado  de " + N + " * " + M + " es de: " + resultado ); 
+            resultado = N / M;
+            System.out.println("El resto de " + N + " / " + M + " es de: " + resultado );
+            resultado = X % Y;
+            System.out.println("El resultado  de " + X + " % " + Y + " es de: " + resultado );
+            resultado = X % N;
+            System.out.println("El resultado  de " + X + " % " + N + " es de: " + resultado );
+            resultado = Y / M;
+            System.out.println("El resto  de " + Y + " / " + M + " es de: " + resultado );
+            resultado = Y % M;
+            System.out.println("El resultado  de " + Y + " % " + M + " es de: " + resultado );
+            resultado = Y;
+            System.out.println("El doble de " + Y + " es de: " + Y * 2);
+            resultado = X;
+            System.out.println("El doble de " + X + " es de: " + X * 2);
+            resultado = N;
+            System.out.println("El doble de " + N + " es de: " + N * 2);
+            resultado = M;
+            System.out.println("El doble de " + M + " es de: " + M * 2);
+            resultado = Y + X + N + M;
+            System.out.println("La suma de todas las variables es de: " + resultado );
+            resultado = Y * X * N * M;
+            System.out.println("El producto de todas las variables es de: " + resultado);
+            
+            
+            
+    /* 
+
+
+
+*/
+            
+            
+            
+    }
+    
+}
